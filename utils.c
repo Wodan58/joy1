8f2a381363b4f283df1d8a1642fddd56f68b9e95
Wodan58 <r.wiersma26@gmail.com>
2024-04-16 08:59:46 +0200

    Patch Tuesday - D

--- utils.c
+++ utils.c
@@ -1,18 +1,22 @@
 /* FILE: utils.c */
 /*
  *  module  : utils.c
- *  version : 1.44
- *  date    : 03/21/24
+ *  version : 1.45
+ *  date    : 04/09/24
  */
 #include "globals.h"
 
 /*
- * newnode - allocate a new node or die trying.
+ * newnode - allocate a new node or die trying. If there is a maximum number of
+ *           nodes, an error will be generated if more nodes are requested than
+ *           the maximum.
  */
-Index newnode(pEnv env, Operator o, Types u, Node *r)
+Index newnode(pEnv env, Operator o, Types u, Index r)
 {
     Node *p;
 
+    if (env->maxnodes && env->nodes >= env->maxnodes)
+	fatal("memory excedes maxnodes");
     if ((p = GC_malloc(sizeof(Node))) == 0)
 	execerror("memory", "allocator");
     p->op = o;
